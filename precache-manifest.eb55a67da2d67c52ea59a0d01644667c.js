b6df0ac891120ba6e8cd9fc76a49ed02c0d7e1e5
HollosJ <hollosj99@gmail.com>
2020-09-06 21:24:58 +0100

    Updates

---
+++ precache-manifest.eb55a67da2d67c52ea59a0d01644667c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe799a309945b86e3df828789c2d3f26",
+    "url": "/card-slider/index.html"
+  },
+  {
+    "revision": "a7ab7a016ec15353b0a5",
+    "url": "/card-slider/static/css/main.da97bd46.chunk.css"
+  },
+  {
+    "revision": "363e2e47e6955311e12d",
+    "url": "/card-slider/static/js/2.155df96e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-slider/static/js/2.155df96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ab7a016ec15353b0a5",
+    "url": "/card-slider/static/js/main.66981c57.chunk.js"
+  },
+  {
+    "revision": "d60ac0d79d5ae454acfa",
+    "url": "/card-slider/static/js/runtime-main.b13f17bb.js"
+  }
+]);
